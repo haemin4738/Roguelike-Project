9412d298e678ab34434e8ab08891178935f17fd5
haemin4738 <leeheamin12@gmail.com>
2026-09-04 16:48:13 +0900

    💄 design: 하트 HP 표시, AbilityData 에셋, 씬 Inspector 연결
    - HeartDisplay.cs: 하트 기반 HP 표시 (full/half/empty 스프라이트)
    - HUDController: Image fillAmount → HeartDisplay.SetHearts() 방식으로 변경
    - AbilityData .asset 6종 추가 (분노/신속/인내/신비/탐욕/집중)
    - Game.unity, MainMenu.unity: Inspector 슬롯 연결 및 UI 레이아웃 설정
    - TextMesh Pro 필수 리소스 추가

---
+++ Assets/Scripts/UI/HeartDisplay.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartDisplay : MonoBehaviour
+{
+    [SerializeField] Sprite fullHeart;
+    [SerializeField] Sprite halfHeart;
+    [SerializeField] Sprite emptyHeart;
+    [SerializeField] Vector2 heartSize = new(32, 32);
+    [SerializeField] int hpPerHeart = 20;
+
+    readonly List<Image> _hearts = new();
+
+    public void SetHearts(float current, float max)
+    {
+        int count = Mathf.Max(1, Mathf.CeilToInt(max / hpPerHeart));
+        SyncCount(count);
+        for (int i = 0; i < count; i++)
+        {
+            float segment = current - i * hpPerHeart;
+            if (segment >= hpPerHeart)
+                _hearts[i].sprite = fullHeart;
+            else if (segment >= hpPerHeart * 0.5f)
+                _hearts[i].sprite = halfHeart;
+            else
+                _hearts[i].sprite = emptyHeart;
+        }
+    }
+
+    void SyncCount(int count)
+    {
+        while (_hearts.Count < count)
+        {
+            var go = new GameObject("Heart", typeof(Image));
+            go.transform.SetParent(transform, false);
+            var img = go.GetComponent<Image>();
+            img.sprite = emptyHeart;
+            go.GetComponent<RectTransform>().sizeDelta = heartSize;
+            _hearts.Add(img);
+        }
+        while (_hearts.Count > count)
+        {
+            Destroy(_hearts[^1].gameObject);
+            _hearts.RemoveAt(_hearts.Count - 1);
+        }
+    }
+}
